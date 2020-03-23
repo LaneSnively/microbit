d262cf32cf3f280791c79bebd4d4b75ce201c6c3
Lane Snively <lsnively@uwyo.edu>
2020-03-23 01:03:03 -0600

    Update buttonExample.py

--- buttonExample.py
+++ buttonExample.py
@@ -5,7 +5,14 @@
 
 from microbit import *
 
-a=1
+while True:
+    cool()
+    game()
+    lane()
+    snively()
+
+def cool():
+    a=1
     for w in range(100):
         for l in range(4):
             display.set_pixel(l,w%5,a)
@@ -16,53 +23,49 @@ a=1
             a+=1
             if(a==10):
                 a=1
-
-x=0
-y=0
-time=0
-while time < 500:
-    time+=1
-    if button_a.is_pressed():
-        x-=1
-    if button_b.is_pressed():
-        x+=1
-    if button_a.is_pressed() and button_b.is_pressed():
-        y+=1
-    if not button_a.is_pressed() and not button_b.is_pressed():
-        y-=1
-    if x>4:
-        x=0
-    if x<0:
-        x=4
-    if y>4:
-        y=0
-    if y<0:
-        y=4
-    display.set_pixel(x,y,9)
-    sleep(85)
-    display.clear()
-
-h=0
-while h<5:
-    display.set_pixel(h,h,9)
-    for i in range(0,5):
-        display.set_pixel(h,i,9)
-    display.clear()
-    i+=1
     
-while True:
+def game():
+    x=0
+    y=0
+    time=0
+    while time < 500:
+        time+=1
+        if button_a.is_pressed():
+            x-=1
+        if button_b.is_pressed():
+            x+=1
+        if button_a.is_pressed() and button_b.is_pressed():
+            y+=1
+        if not button_a.is_pressed() and not button_b.is_pressed():
+            y-=1
+        if x>4:
+            x=0
+        if x<0:
+            x=4
+        if y>4:
+            y=0
+        if y<0:
+            y=4
+        display.set_pixel(x,y,9)
+        sleep(85)
+        display.clear()
+        
+def lane():
     display.set_pixel(0,0,9)
     display.clear()
+    sleep(85)
     display.set_pixel(0,1,9)
     display.set_pixel(1,1,9)
     display.set_pixel(1,0,9)
     display.clear()
+    sleep(85)
     display.set_pixel(0,2,9)
     display.set_pixel(1,2,9)
     display.set_pixel(2,2,9)
     display.set_pixel(2,1,9)
     display.set_pixel(2,0,9)
     display.clear()
+    sleep(85)
     display.set_pixel(0,3,9)
     display.set_pixel(1,3,9)
     display.set_pixel(2,3,9)
@@ -71,6 +74,7 @@ while True:
     display.set_pixel(3,1,9)
     display.set_pixel(3,0,9)
     display.clear()
+    sleep(85)
     display.set_pixel(0,4,9)
     display.set_pixel(1,4,9)
     display.set_pixel(2,4,9)
@@ -80,3 +84,14 @@ while True:
     display.set_pixel(4,2,9)
     display.set_pixel(4,1,9)
     display.set_pixel(4,0,9)
+    display.clear()
+    sleep(85)
+    
+def snively():
+    h=0
+    while h<5:
+        display.set_pixel(h,h,9)
+        for i in range(0,5):
+            display.set_pixel(h,i,9)
+            display.clear()
+            
